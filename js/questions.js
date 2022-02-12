d15f9bc87802225cc9bb6d388cf53c5751801d50
bwalidd <bouwachmawalid1@gmail.com>
2022-02-12 21:33:21 +0100

    first commit

---
+++ js/questions.js
@@ -0,0 +1,118 @@
+ // création d'un tableau et transmission du nombre, des questions, des options et des réponses
+let questions = [
+    {
+    numb: 1,
+    question: "  En gestion de projet qui appelle-t-on maîtrise d'ouvrage?",
+    answer: "la société chargée de l'accompagnement",
+    options: [
+      " le client",
+      "le prestataire",
+      "la société chargée de l'accompagnement",
+      "les utilisateurs"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quel code de réponse HTTP 1.0 est faux?",
+    answer: "500 : requête acceptée, traitement en cours ",
+    options: [
+      "200 : la requête s'est déroulée correctement",
+      "400 : erreur de syntaxe dans la requête envoyé par le client",
+      "500 : requête acceptée, traitement en cours ",
+      "404 : la ressource demandée n'existe pas"
+    ]
+  },
+    {
+    numb: 3,
+    question: "UML est ?",
+    answer: "un langage de modélisation",
+    options: [
+      "un langage de modélisation",
+      "un type de port",
+      " La partie « données » de la méthode MERISE",
+      "un standard de communication"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Sous Windows XP, la configuration est enregistré dans  ?",
+    answer: "La base de registre",
+    options: [
+      "La base de composants de windows",
+      "Le fichier win.ini",
+      "La base de registre",
+      "Le fichier autoexec.bat"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Une URL (Uniform Resource Locator) est composé obligatoirement de certains éléments, ci dessous, un est facultatif :",
+    answer: "du port",
+    options: [
+      "du port",
+      "du nom du serveur",
+      "cdu protocole de communication",
+      "tous les response faux"
+    ]
+  },
+  
+   {
+   numb: 6,
+   question: "On considère la proposition suivante: 'Je passerai mes vacances d'été en Egypte ou en Turquie'. La négation de cette proposition est:",
+   answer: "Je ne passerai pas mes vacances d'été en Egypte ni en Turquie.",
+   options: [
+       "Je ne passerai pas mes vacances d'été en Egypte ou en Turquie.",
+       "Je ne passerai pas mes vacances d'été en Egypte ni en Turquie.",
+       "Je ne partirai pas pendant les vacances d'été.",
+       "Je ne passerai pas mes vacances d'hiver en Egypte ou en Turquie."
+   ]
+   },
+   
+   {
+    numb: 7,
+    question: "Soit F une fonction de R dans R  .On considère la proposition suivante: 'est une fonction positive' La négation de cette proposition est:",
+    answer: "F n'est pas une fonction positive.",
+    options: [
+        "F n'est pas une fonction positive.",
+        "F est une fonction négative.",
+        "F n'est pas une fonction.",
+        "F est une fonction ni positive, ni négative."
+    ]
+    },
+    
+   {
+    numb: 8,
+    question: "On considère la proposition suivante: 's'il fait beau, j'irai à la plage'.La négation de cette proposition est:",
+    answer: "Il fait beau et je ne vais pas à la plage.",
+    options: [
+        "S'il fait beau, je n'irai pas à la plage.",
+        "S'il ne fait pas beau, j'irai à la plage.",
+        "Il fait beau et je ne vais pas à la plage.",
+        "S'il ne fait pas beau, je n'irai pas à la plage."
+    ]
+    },
+    
+   {
+    numb: 9,
+    question: "On considère la proposition P : 'Les personnes qui parlent trop ne réfléchissent pas souvent'.Parmi les propositions suivantes, laquelle est équivalente à P ?",
+    answer: "Les personnes qui réfléchissent souvent ne parlent pas trop.",
+    options: [
+        "Les personnes qui parlent trop ne réfléchissent pas.",
+        "Les personnes qui réfléchissent souvent ne parlent pas trop.",
+    "Les personnes qui réfléchissent souvent parlent trop.",
+    "Les personnes qui ne parlent pas trop réfléchissent souvent."
+    ]
+    },
+    
+   {
+    numb: 10,
+    question: "(Un) est une suite arithmétique de premier terme U1=1 et de raison 2. Alors,",
+    answer: "U1+U2+U3+U4=16",
+    options: [
+        "U5=1",
+        "U12=22",
+        "U1+U2+U3+U4=16",
+        "U7+U8+U9+U10=48"
+    ]
+    },
+];
